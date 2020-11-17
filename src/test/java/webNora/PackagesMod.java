7b5a2eee06bd64fb115b46d05d3178a9409b1b1f
adrushchyts <a.drushchyts@setplex.com>
2020-11-17 17:52:17 +0300

    commit

--- src/test/java/webNora/PackagesMod.java
+++ src/test/java/webNora/PackagesMod.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class PackagesModTest {
+public class PackagesMod {
 
     AbstractHelper abstractHelper = new AbstractHelper();
     AddPackageHelper addPackageHelper = new AddPackageHelper();
