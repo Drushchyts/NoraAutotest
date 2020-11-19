669135b9ab871088a45b4af897f26e996185e158
adrushchyts <a.drushchyts@setplex.com>
2020-11-19 17:30:52 +0300

    commit

--- src/main/java/webNora/automation/core/utils/BrowserConstants.java
+++ src/main/java/webNora/automation/core/utils/BrowserConstants.java
@@ -1,6 +1,7 @@
 package webNora.automation.core.utils;
 
 public class BrowserConstants {
-    public static final String CHROMIUM = "chromium";
+
+    public static final String CHROMIUM = "Chromium";
     public static final String FIREFOX = "firefox";
 }
