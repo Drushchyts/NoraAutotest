6515ae4c0b79f873958c5021ec74c85ae9c3fe2b
adrushchyts <a.drushchyts@setplex.com>
2020-11-23 15:59:46 +0300

    commit

--- src/test/java/webNora/Dasdafqf.java
+++ src/test/java/webNora/Dasdafqf.java
@@ -1,16 +1,19 @@
 package webNora;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class Test {
 
+public class Dasdafqf {
 
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void createPackage() {
         driver.get("https://google.com");
         driver.close();
     }
-
 
 }
