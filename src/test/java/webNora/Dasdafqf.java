8fc529521f7fc27f2f07584a535c7d356c7f4b77
adrushchyts <a.drushchyts@setplex.com>
2020-11-23 16:39:43 +0300

    commit

--- src/test/java/webNora/Dasdafqf.java
+++ src/test/java/webNora/Dasdafqf.java
@@ -3,16 +3,24 @@ package webNora;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class Dasdafqf {
-    private static WebDriver driver;
+    private static WebDriver driver = null;
 
     @BeforeMethod
     public void startUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless"); //!!!should be enabled for Jenkins
+        options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
+        options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+        driver = new ChromeDriver(options);
+
     }
 
     @Test
