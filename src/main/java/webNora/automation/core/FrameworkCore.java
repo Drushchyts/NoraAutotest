1cdd130da0d1b3e4852d1a20a03194efdbd02070
adrushchyts <a.drushchyts@setplex.com>
2020-11-23 17:30:05 +0300

    commit

--- src/main/java/webNora/automation/core/FrameworkCore.java
+++ src/main/java/webNora/automation/core/FrameworkCore.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import webNora.automation.core.utils.BrowserConstants;
 import webNora.automation.core.utils.IOUtils;
 
+import java.util.Collections;
+
 public class FrameworkCore {
 
     public static WebDriver webDriver;
@@ -19,10 +21,13 @@ public class FrameworkCore {
     public static WebDriver getInstance() {
         if (browser.equals(BrowserConstants.CHROMIUM)) {
             ChromeOptions options = new ChromeOptions();
+            options.setBinary("/snap/chromium/1411/usr/lib/chromium-browser/chrome");  //chrome binary location specified here
             options.addArguments("--no-sandbox");
             options.addArguments("--headless"); //!!!should be enabled for Jenkins
             options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
             options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+            options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+            options.setExperimentalOption("useAutomationExtension", false);
             webDriver = new ChromeDriver(options);
 
         } else {
