50cdc2e6532c19e747edc60acc034a60b9614efc
adrushchyts <a.drushchyts@setplex.com>
2020-11-23 17:01:24 +0300

    commit

--- src/test/java/webNora/Dasdafqf.java
+++ src/test/java/webNora/Dasdafqf.java
@@ -11,20 +11,16 @@ import org.testng.annotations.Test;
 public class Dasdafqf {
     private static WebDriver driver = null;
 
-    @BeforeMethod
-    public void startUp() {
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--no-sandbox");
-        //options.addArguments("--headless"); //!!!should be enabled for Jenkins
-        options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
-        options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
-        driver = new ChromeDriver(options);
-
-    }
 
     @Test
     public void dasdasdasd() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless"); //!!!should be enabled for Jenkins
+        options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
+        options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+        driver = new ChromeDriver(options);
         driver.get("https://google.com");
         driver.close();
     }
