988189ec0e34a8da7d402af9e9c9c0dcc0c47089
adrushchyts <a.drushchyts@setplex.com>
2020-11-23 17:11:54 +0300

    commit

--- src/test/java/webNora/Dasdafqf.java
+++ src/test/java/webNora/Dasdafqf.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+
 
 public class Dasdafqf {
     private static WebDriver driver = null;
@@ -15,11 +17,14 @@ public class Dasdafqf {
     @Test
     public void dasdasdasd() {
 
+        //System.setProperty("webdriver.chrome.driver", "chromium-browser.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--headless"); //!!!should be enabled for Jenkins
         options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
         options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
         driver = new ChromeDriver(options);
         driver.get("https://google.com");
         driver.close();
