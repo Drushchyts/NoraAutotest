447bbe0dac0996ea32a6aa041c9317602b3bac89
adrushchyts <a.drushchyts@setplex.com>
2020-11-25 15:19:56 +0300

    commit

--- src/main/java/webNora/automation/core/FrameworkCore.java
+++ src/main/java/webNora/automation/core/FrameworkCore.java
@@ -22,12 +22,12 @@ public class FrameworkCore {
         if (browser.equals(BrowserConstants.CHROMIUM)) {
             ChromeOptions options = new ChromeOptions();
             options.setBinary("/snap/chromium/1411/usr/lib/chromium-browser/chrome");  //chrome binary location specified here
-//            options.addArguments("--no-sandbox");
-//            options.addArguments("--headless"); //!!!should be enabled for Jenkins
-//            options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
-//            options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
-//            options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-//            options.setExperimentalOption("useAutomationExtension", false);
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless"); //!!!should be enabled for Jenkins
+            options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
+            options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+            options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+            options.setExperimentalOption("useAutomationExtension", false);
             webDriver = new ChromeDriver(options);
 
         } else {
