61d19c20f73719a79f51aa574cf9470952d9bb1c
adrushchyts <a.drushchyts@setplex.com>
2020-12-01 12:42:12 +0300

    44 test finish

--- src/test/java/webNora/PackagesModTest.java
+++ src/test/java/webNora/PackagesModTest.java
@@ -1,5 +1,6 @@
 package webNora;
 
+import jdk.jfr.Description;
 import webNora.automation.core.FrameworkCore;
 import webNora.automation.core.utils.IOUtils;
 import webNora.helpers.AbstractHelper;
@@ -35,44 +36,44 @@ public class PackagesModTest extends FrameworkCore {
     @Test
     public void createPackage() {
         addPackageHelper.clickPackageButton();
-//                .createPackage();
+                .createPackage();
     }
 
-//    @Test(dependsOnMethods = "createPackage", skipFailedInvocations = true)
-//    public void activationSettings() throws InterruptedException {
-//        addActivationSettingsHelper.paymentSets()
-//                .paymentServices()
-//                .currencies()
-//                .currencyConverters()
-//                .generalSettings();
-//    }
-//
-//    @Test(dependsOnMethods = "activationSettings")
-//    public void contentSets() {
-//        contentSetsHelper.clickCouponsButton();
-//    }
-//
-//    @Test(dependsOnMethods = "contentSets")
-//    public void coupons() {
-//        addCouponsHelper.clickCouponsButton();
-//    }
-//
-//    @Test(dependsOnMethods = "coupons", skipFailedInvocations = true)
-//    public void stbSettings() throws InterruptedException {
-//        stbSettingsHelper.addStbSettings();
-//    }
-//
-//    @Test(dependsOnMethods = "stbSettings", skipFailedInvocations = true)
-//    public void stbChannelList() {
-//        stbChannelListHelper.clickSTBChannelList();
-//    }
-//
-//    @Test(dependsOnMethods = "stbChannelList", skipFailedInvocations = true)
-//    public void announcementsAndReminders() throws InterruptedException {
-//        annAndRemindersHelper.addAnnouncement()
-//                .addReminder()
-//                .activationTOA();
-//    }
+    @Test(dependsOnMethods = "createPackage", skipFailedInvocations = true)
+    public void activationSettings() throws InterruptedException {
+        addActivationSettingsHelper.paymentSets()
+                .paymentServices()
+                .currencies()
+                .currencyConverters()
+                .generalSettings();
+    }
+
+    @Test(dependsOnMethods = "activationSettings")
+    public void contentSets() {
+        contentSetsHelper.clickCouponsButton();
+    }
+
+    @Test(dependsOnMethods = "contentSets")
+    public void coupons() {
+        addCouponsHelper.clickCouponsButton();
+    }
+
+    @Test(dependsOnMethods = "coupons", skipFailedInvocations = true)
+    public void stbSettings() throws InterruptedException {
+        stbSettingsHelper.addStbSettings();
+    }
+
+    @Test(dependsOnMethods = "stbSettings", skipFailedInvocations = true)
+    public void stbChannelList() {
+        stbChannelListHelper.clickSTBChannelList();
+    }
+
+    @Test(dependsOnMethods = "stbChannelList", skipFailedInvocations = true)
+    public void announcementsAndReminders() throws InterruptedException {
+        annAndRemindersHelper.addAnnouncement()
+                .addReminder()
+                .activationTOA();
+    }
 
     @AfterClass
     public void tearDown() {
